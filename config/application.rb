d1fb5d3ce1e2ad250c14dad710f076add09cbff6
raphael.rodrigues <raphaeljr28@gmail.com>
2013-11-06 13:58:17 +0000

    first commit

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,12 @@ module LandingAgroSocial
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.assets.initialize_on_precompile = false
+    
     config.assets.precompile += ["fontawesome-webfont.ttf",
                          "fontawesome-webfont.eot",
                          "fontawesome-webfont.svg",
                          "fontawesome-webfont.woff"]
+
   end
 end
