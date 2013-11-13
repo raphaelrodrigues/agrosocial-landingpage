5c7229ad5154c5e235b25a6266debdfeca452fba
Miguel Guimarães <hmtlguimaraes@gmail.com>
2013-11-13 10:42:10 +0000

    mais 23423

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,26 +15,7 @@
 //= require turbolinks
 //= require_tree .
 
-$.fn.isOnScreen = function(){
-    
-    var win = $(window);
-    
-    var viewport = {
-        top : win.scrollTop(),
-        left : win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
-    
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
-    
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-    
-};
-
-  function scrollToPosition(id) {
+function scrollToPosition(id) {
     if (id !== undefined) {
         $('html,body').animate({
           scrollTop: $('#'+id).offset().top},
